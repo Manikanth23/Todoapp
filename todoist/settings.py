4b11eb4365ff8071a10d0c57aaa898c12d06220f
Manikanth23 <bongonimanikanth@gmail.com>
2022-04-20 18:06:44 +0530

    committed

--- todoist/settings.py
+++ todoist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+2=)!y)7%%iiqp(0-8i@k!sh@b07moz0cc@k7bc0_$zwj*#3at'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['guarded-gorge-16884.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['guarded-gorge-16884.herokuapp.com','127.0.0.1']
 
 
 # Application definition
